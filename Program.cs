fdbc0f44443ae9bf537433ac8f8f221b0ff9aa2b
mattruma <mattruma@gmail.com>
2020-11-23 11:22:54 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJR076
+{
+    class Program
+    {
+        // https://www.c-sharpcorner.com/article/azure-event-hub-implementation-using-net-core-console-app/
+        // https://www.thecodebuzz.com/parse-command-line-argument-system-commandline/
+
+        protected static IConfiguration _configuration;
+
+        public static async Task Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Option<int>(
+                    "--number-of-batches",
+                    "The number of batches to send."),
+                new Option<int>(
+                    "--number-of-messages",
+                    "The number of messages to send per batch.")
+            };
+
+            rootCommand.Description = "Console App to load test an Azure Event Hub.";
+
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            Console.ResetColor();
+            Console.WriteLine("Event Hub Load Tester");
+
+            rootCommand.Handler = CommandHandler.Create<int, int>(ExecuteAsync);
+
+            await rootCommand.InvokeAsync(args);
+
+            Console.ReadKey();
+        }
+
+        public static async void ExecuteAsync(
+            int numberOfBatches,
+            int numberOfMessages)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine();
+            Console.WriteLine("# Batches            : {0:N0}", numberOfBatches);
+            Console.WriteLine("# Messages/Batch     : {0:N0}", numberOfMessages);
+            Console.WriteLine("# Messages           : {0:N0}", numberOfBatches * numberOfMessages);
+            Console.WriteLine();
+
+            var eventHubProducerClient =
+                new EventHubProducerClient(
+                    _configuration["EventHub_PrimaryConnectionString"],
+                    _configuration["EventHub_Name"]);
+
+            var message = await File.ReadAllTextAsync("sample.json");
+
+            for (var batchIndex = 0; batchIndex < numberOfBatches; batchIndex++)
+            {
+                var partitionId = 0;
+
+                var createBatchOptions =
+                    new CreateBatchOptions()
+                    {
+                        PartitionId = partitionId.ToString()
+                    };
+
+                var eventDataBatch =
+                    await eventHubProducerClient.CreateBatchAsync(createBatchOptions);
+
+                for (var messageIndex = 0; messageIndex < numberOfMessages; messageIndex++)
+                {
+                    var eventData =
+                        new EventData(
+                            Encoding.UTF8.GetBytes(message));
+
+                    eventDataBatch.TryAdd(eventData);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Sending batch {0:N0} with {1:N0} message(s)...", batchIndex + 1, numberOfMessages);
+
+                await eventHubProducerClient.SendAsync(
+                    eventDataBatch);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Batch sent.");
+                Console.WriteLine();
+
+                partitionId++;
+
+                if (partitionId > 1) _ = 0;
+            }
+
+            Console.ResetColor();
+            Console.Write("Press any key to exit...");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# Introduction
+
+Console App to send messages to an Event Hub.
+
+## Setup
+
+For the Console App to run you need access to an Event Hub in Azure.
+
+Update the `appsettings.json` file.
+
+```json
+{
+  "EventHub_Name": "",
+  "EventHub_PrimaryConnectionString": "",
+  "EventHub_SecondaryConnectionString": ""
+}
+```
+
+* **EventHub_Name** - the name of the Event Hub to send messages.
+* **EventHub_PrimaryConnectionString** - the primary connection string of the Event Hub, e.g. `Endpoint=sb://EVENT_HUB_NAMESPACE.servicebus.windows.net/;SharedAccessKeyName=SAS_NAME;SharedAccessKey=SAS_KEY;` 
+
+Note, the `EventHub_SecondaryConnectionString` is not required yet.
+
+## Execution
+
+To run the Consol App, open in VS Code and at the terminal prompt enter `dotnet run --number-of-batches 2 --number-of-messages 100`.
+
+This will send (2) batches with a (100) messages in each batch.
+
+I did try getting this to work with .NET 5.0 but ran into an isse with `CreateBatchOptions` method of the `EventHubProducerClient` client. Kept throwing an exception.
